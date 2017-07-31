cfe908e89706cc5963c8502d53408f48b08f31af
jneidel <jneidel@outlook.com>
2017-07-31 20:41:22 +0200

    Add manifest.json
    Clicking the icon and opening a new tab wil open the app.

---
+++ app/background.js
@@ -0,0 +1,28 @@
+chrome.browserAction.onClicked.addListener(openNewTab);
+chrome.tabs.onCreated.addListener(openApp);
+
+function openNewTab() {
+    let tabProperties = {
+        url: "index.html"
+    }
+    chrome.tabs.create(tabProperties);
+}
+
+function openApp() {
+    let updateProperties = {
+        url: "index.html"
+	}
+	let tabProperties = {
+		currentWindow: true, 
+		active : true
+	}
+	function checkIfNewTab(tabData) {
+		let url = tabData[0].url;
+		if (url === "chrome://newtab/") {
+			chrome.tabs.update(updateProperties)
+		}
+	}
+	chrome.tabs.query(tabProperties, function(data){
+		checkIfNewTab(data); //Direct call doesn't work
+	})
+}
