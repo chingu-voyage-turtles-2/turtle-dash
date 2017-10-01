0a77cebb6c61a3a41643b86fa2858551314ddd3b
jneidel <jneidel@outlook.com>
2017-10-01 18:39:49 +0200

    Update readability with eslint

--- app/background.js
+++ app/background.js
@@ -1,31 +1,33 @@
-"use strict;"
+"use strict;";
 
-chrome.browserAction.onClicked.addListener(openNewTab);
-chrome.tabs.onCreated.addListener(openApp);
+/* globals chrome */
+
+chrome.browserAction.onClicked.addListener( openNewTab );
+chrome.tabs.onCreated.addListener( openApp );
 
 function openNewTab() {
-    let tabProperties = {
-        url: "index.html"
-    }
-    chrome.tabs.create(tabProperties);
+    const tabProperties = {
+        url: "index.html",
+    };
+    chrome.tabs.create( tabProperties );
 }
 
 function openApp() {
-    let updateProperties = {
-        url: "index.html"
-    }
-    let tabProperties = {
+    const updateProperties = {
+        url: "index.html",
+    },
+    tabProperties = {
         currentWindow: true,
-        active : true
-    }
-    function checkIfNewTab(tabData) {
-        let url = tabData[0].url;
-        if (url === "chrome://newtab/") {
-            chrome.tabs.update(updateProperties)
-            window.history.replaceState({} , "", " ");
+        active       : true,
+    };
+    function checkIfNewTab( tabData ) {
+        const url = tabData[0].url;
+        if ( url === "chrome://newtab/" ) {
+            chrome.tabs.update( updateProperties );
+            window.history.replaceState( {}, "", " " );
         }
     }
-    chrome.tabs.query(tabProperties, function(data){
-        checkIfNewTab(data); //Direct call doesn't work
-    })
+    chrome.tabs.query( tabProperties, ( data ) => {
+        checkIfNewTab( data ); // Direct call doesn't work
+    } );
 }
